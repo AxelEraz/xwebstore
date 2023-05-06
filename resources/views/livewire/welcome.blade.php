3c28eeba1f8c7f643269f3f8bd4af729830efdac
AxelEraz <axeleraz5@gmail.com>
2023-05-06 18:50:49 -0300

    V0.2

---
+++ resources/views/livewire/welcome.blade.php
@@ -0,0 +1,16 @@
+<div class="grid grid-cols-1 lg:grid-cols-2">
+    
+    <div class="px-auto mx-auto mt-5 text-center">
+        <h1 class="mb-4 text-3xl font-extrabold text-gray-900 md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Bienvenido al futuro!</span> <br> Te invito a desarrollar tu negocio on-line.</h1>
+        <p class="text-lg font-normal text-gray-500 lg:text-xl">Solo necesitas estar a la altura para competir con los referentes de tu rubro</p>
+    </div>    
+    <div class="px-auto mx-auto mt-5">
+        <livewire:contact-form/>
+    </div>
+    
+    <div class="px-auto mx-auto mt-5">
+        <a href="">
+            
+        </a>
+    </div>
+</div>
