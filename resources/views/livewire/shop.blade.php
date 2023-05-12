dca02bdfd2620489dd7c6cade6eb5fd2b57ba721
AxelEraz <axeleraz5@gmail.com>
2023-05-11 23:11:03 -0300

    V0.4

--- resources/views/livewire/shop.blade.php
+++ resources/views/livewire/shop.blade.php
@@ -1,3 +1,39 @@
 <div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
+
+
+    <div class="relative overflow-x-auto">
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 my-10 py-10">
+            <tbody>
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        Tittle
+                    </th>
+                    <td class="px-6 py-4">
+                        Slug
+                    </td>
+                    <td class="px-6 py-4">
+                        Deliver
+                    </td>
+                    <td class="px-6 py-4">
+                        Price
+                    </td>
+                </tr>
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        Microsoft Surface Pro
+                    </th>
+                    <td class="px-6 py-4">
+                        White
+                    </td>
+                    <td class="px-6 py-4">
+                        Laptop PC
+                    </td>
+                    <td class="px-6 py-4">
+                        $1999
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
 </div>
--- resources/views/livewire/welcome.blade.php
+++ resources/views/livewire/welcome.blade.php
@@ -1,8 +1,10 @@
 <div>
+
+    <!-- Caption & Contact form -->
     <div class="grid grid-cols-1 lg:grid-cols-2 mt-10">
         
         <div class="px-auto ml-10 mt-5 text-center">
-            <h1 class="mb-4 text-3xl font-extrabold dark:text-gray-200 text-gray-900 md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Bienvenido al futuro!</span> <br> Te invito a desarrollar tu negocio on-line.</h1>
+            <h1 class="mb-4 text-3xl font-extrabold dark:text-gray-200 text-gray-900 md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">¡Bienvenido al futuro!</span> <br> Te invito a desarrollar tu negocio on-line.</h1>
             <p class="text-lg font-normal text-gray-500 dark:text-gray-300 lg:text-xl">Solo necesitas estar a la altura para competir con los referentes de tu rubro</p>
         </div>    
         <div class="px-auto mx-auto mt-5">
@@ -11,6 +13,7 @@
     
     </div>
 
+    <!-- Caption-->
     <div class="grid grid-cols-1 lg:grid-cols-2 items-center mt-20">
         
         <div class="mx-auto">
@@ -25,10 +28,12 @@
         </div>
 
     </div>
+
+    <!-- Free services -->
     
     <div class="grid grid-cols-1 lg:grid-cols-3 items-center py-10 mt-20 dark:bg-slate-600">
         
-        <!-- -->
+        <!-- Shop -->
         <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mx-auto py-5 px-5">
             <a href="#">
                     <x-shop />
@@ -44,39 +49,58 @@
                 </a>
             </div>
         </div>
-        <!-- -->
+        <!-- X-List -->
         <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mx-auto py-5 px-5">
             <a href="#">
-                    <x-grow class="" />
+                    <x-provider class="" />
             </a>
             <div class="p-5">
                 <a href="#">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-100">Publica en nuestra shop online</h5>
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-100 text-center">X-list</h5>
                 </a>
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"></p>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-300">Si tenes una distrbuidora o sos proveedor de insumos necesitas probar esta herramienta con tus clientes!</p>
                 <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Read more
+                    Probar ahora!
                     <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                 </a>
             </div>
         </div>
 
-        <!-- -->
+        <!-- Content -->
         <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mx-auto py-5 px-5">
             <a href="#">
-                    <x-grow class="" />
+                    <x-rrss class="" />
             </a>
             <div class="p-5">
                 <a href="#">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-100"></h5>
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-100 text-center">Redes sociales</h5>
                 </a>
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"></p>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-300">Contenido gratis para llevar a otro nivel las redes de tu negocio con prácticas profesionales!</p>
                 <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Read more
+                    Iniciar curso!
                     <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                 </a>
             </div>
         </div>
 
     </div>
+
+    <!-- Paid services -->
+    
+    <div class="grid grid-cols-1 lg:grid-cols-2 items-center mt-20 mb-10">
+        
+        <div class="text-center mx-auto">
+
+            <h5 class="mb-4 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-3xl dark:text-white ml-5">SOMOS LA MEJOR
+            <span class="text-blue-600 dark:text-blue-500">INVERSIÓN</span> PARA EXPANDIRTE Y BRINDARTE EL CRECIMIENTO QUE BUSCAS.</h5>
+            <h5 class="mb-4 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-3xl dark:text-white ml-5">
+            <span class="text-blue-600 dark:text-blue-500">CONOCÉ </span>NUESTROS SERVICIOS WEB PROFESIONALES Y LLEVA AL LIMITE TUS GANANCIAS</h5>       
+        </div>
+
+        <div class="mx-auto">
+            <x-limit />
+        </div>
+
+    </div>
+
 </div>
